94058cc7df0af6a687060519011ac57e0bd6627f
LAB23_USER ON WS43 <L23X14W43@citulab.net>
2025-02-14 13:05:55 +0800

    Added Hello World API in Spring Boot

---
+++ backend/audioscholar/src/main/java/edu/cit/audioscholar/AudioscholarApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.audioscholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioscholarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioscholarApplication.class, args);
+	}
+
+}
---
+++ backend/audioscholar/src/test/java/edu/cit/audioscholar/AudioscholarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.audioscholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioscholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
